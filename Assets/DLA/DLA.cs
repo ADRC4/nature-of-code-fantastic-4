e9534710c9de76b60fc2538731772a8b6e1c632a
Vicente <vicente@visose.com>
2017-12-17 14:26:46 +0000

    Add project files.

---
+++ Assets/DLA/DLA.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DLA
+{
+    public int[,] Cells;
+    public int GenerationCount = 0;
+    private int _width;
+    private int _height;
+
+    public DLA(int width, int height)
+    {
+        _width = width;
+        _height = height;
+
+        Cells = new int[_width, _height];
+
+        for (int y = 0; y < _height; y++)
+            for (int x = 0; x < _width; x++)
+            {
+                bool r = Random.value < 0.01f;
+                if (r) Cells[x, y] = 1;
+            }
+
+        Cells[_width / 2, _height / 2] = -1;
+    }
+
+    public void NextGeneration()
+    {
+        var temp = new int[_width, _height];
+
+        for (int y = 0; y < _height; y++)
+            for (int x = 0; x < _width; x++)
+            {
+                int count = Cells[x, y];
+
+                if (count < 0)
+                {
+                    temp[x, y] = count - 1;
+                }
+                else if (count > 0)
+                {
+                    if (HasStaticNeighbour(x, y))
+                    {
+                        temp[x, y] = -1;
+                    }
+                    else
+                    {
+                        for (int i = 0; i < count; i++)
+                        {
+                            int nx = x + Random.Range(-1, 2);
+                            int ny = y + Random.Range(-1, 2);
+
+                            nx = Wrap(nx, _width);
+                            ny = Wrap(ny, _height);
+
+                            temp[nx, ny] += 1;
+                        }
+                    }
+                }
+            }
+
+        Cells = temp;
+        GenerationCount++;
+    }
+
+    int Wrap(int i, int size)
+    {
+        if (i < 0) return size - 1;
+        if (i > size - 1) return 0;
+        return i;
+    }
+
+    bool HasStaticNeighbour(int x, int y)
+    {
+        for (int xi = -1; xi < 2; xi++)
+            for (int yi = -1; yi < 2; yi++)
+            {
+                if (xi == 0 && yi == 0) continue;
+
+                int nx = x + xi;
+                int ny = y + yi;
+
+                nx = Wrap(nx, _width);
+                ny = Wrap(ny, _height);
+
+                int count = Cells[nx, ny];
+                if (count < 0) return true;
+            }
+
+        return false;
+    }
+}
---
+++ Assets/DLA/DrawImage.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawImage : MonoBehaviour
+{
+    [SerializeField]
+    private GUISkin _skin;
+
+    private Rect _rectangle;
+    private int _width;
+    private int _height;
+    private int _size;
+    private int _generationsPerFrame = 5;
+
+    private Texture2D _image;
+    private Color[] _colors;
+    private DLA _dla;
+
+    private void Start()
+    {
+        Random.InitState(42);
+        int scale = 2;
+        _width = Screen.width / scale;
+        _height = Screen.height / scale;
+        _size = _width * _height;
+
+        _image = new Texture2D(_width, _height)
+        {
+            filterMode = FilterMode.Point
+        };
+
+        _rectangle = new Rect(0, 0, Screen.width, Screen.height);
+        _colors = new Color[_size];
+        _dla = new DLA(_width, _height);
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < _size; i++)
+        {
+            int x = i % _width;
+            int y = i / _width;
+
+            if (_dla.Cells[x, y] > 0)
+            {
+                _colors[i] = Color.white;
+            }
+            else if (_dla.Cells[x, y] < 0)
+            {
+                float f = -_dla.Cells[x, y] / (_dla.GenerationCount + 1f);
+                f = Mathf.Clamp01(f) * 0.25f;
+                _colors[i] = Color.HSVToRGB(f, 1, 1);
+            }
+            else
+            {
+                _colors[i] *= 0.9f;
+                _colors[i].a = 1f;
+            }
+        }
+
+        for (int i = 0; i < _generationsPerFrame; i++)
+        {
+            _dla.NextGeneration();
+        }
+
+        _image.SetPixels(_colors);
+        _image.Apply();
+    }
+
+    private void OnGUI()
+    {
+        GUI.skin = _skin;
+        GUI.DrawTexture(_rectangle, _image);
+        GUILayout.Label("Generations per frame: ");
+        _generationsPerFrame = (int)GUILayout.HorizontalSlider(_generationsPerFrame, 1, 100);
+    }
+}
