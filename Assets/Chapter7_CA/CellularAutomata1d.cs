81a5e2a0c2a5a17f3aeeacbc83c24adf100f4f61
Ngai Wu <ucbqnhw@ucl.ac.uk>
2017-12-30 15:51:14 +0000

    gamecontroller

---
+++ Assets/Chapter7_CA/CellularAutomata1d.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellularAutomata1d : MonoBehaviour
+{
+
+    public bool[] Cells;
+
+    public CellularAutomata1d()
+    {
+        Cells = new bool[41];
+        Cells[20] = true;
+    }
+
+    public void NextGeneration()
+    {
+        int l = Cells.Length;
+        bool[] temp = new bool[1];
+
+        for (int i = 0; i < l; i++)
+        {
+            temp[i] = NextCell(i);
+        }
+
+        Cells = temp;
+
+    }
+    bool NextCell(int i)
+    {
+        int l = Cells.Length;
+        bool cell = Cells[i];
+        bool left = (i == 0) ? Cells[l - 1] : Cells[i - 1];
+        bool right = (i == l - 1) ? Cells[0] : Cells[i + 1];
+
+        if (left && right) return false;
+        if (!cell && (left || right)) return true;
+        if (cell && left) return false;
+
+        return cell;
+    }
+
+}
--- Assets/Chapter7_CA/GameController.cs
+++ Assets/Chapter7_CA/GameController.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public GUISkin UISkin;
+
+    IEnumerator AnimateCellularRow()
+    {
+        var cellular
+    }
+
+    private void OnGUI()
+    {
+        GUI.skin = UISkin;
+
+
+    }
 }
