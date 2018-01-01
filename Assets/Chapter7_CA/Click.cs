36e80f82aeab7c2fe3e95bb883be9fb6ede0c67c
Ngai Wu <ucbqnhw@ucl.ac.uk>
2018-01-01 23:16:53 +0000

    click click

--- Assets/Chapter7_CA/Click.cs
+++ Assets/Chapter7_CA/Click.cs
@@ -6,9 +6,16 @@ public class Click : MonoBehaviour {
 
     [SerializeField]
     private LayerMask clickablesLayer;
-	
-	// Update is called once per frame
-	void Update () {
+
+    private List<GameObject> selectedObjects;
+
+    private void Start()
+    {
+        selectedObjects = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
         if(Input.GetMouseButtonDown(0))
         {
@@ -16,12 +23,41 @@ public class Click : MonoBehaviour {
 
             if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out rayHit, Mathf.Infinity, clickablesLayer))
             {
-
                 ClickOn clickOnScript = rayHit.collider.GetComponent<ClickOn>();
+                if (Input.GetKey("left ctrl"))
+                {
+                    if(clickOnScript.currentlySelected == false)
+                    {
+                        selectedObjects.Add(rayHit.collider.gameObject);
+                        clickOnScript.currentlySelected = true;
+                        clickOnScript.ClickMe();
+                    }
+                    else
+                    {
+                        selectedObjects.Remove(rayHit.collider.gameObject);
+                        clickOnScript.currentlySelected = false;
+                        clickOnScript.ClickMe();
+                    }
+                }
+                else
+                {
+                    if(selectedObjects.Count > 0)
+                    {
+                        foreach (GameObject obj in selectedObjects)
+                        {
+                            obj.GetComponent<ClickOn>().currentlySelected = false;
+                            obj.GetComponent<ClickOn>().ClickMe();
+                        }
 
-                clickOnScript.currentlySelected = !clickOnScript.currentlySelected;
+                        selectedObjects.Clear();
+                    }
 
-                clickOnScript.ClickMe();
+                    selectedObjects.Add(rayHit.collider.gameObject);
+                    clickOnScript.currentlySelected = true;
+                    clickOnScript.ClickMe();
+                }
+                    
+                }
             }
         }
 	}
