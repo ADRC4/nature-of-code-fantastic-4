b8d4d50873cde2dac7ce2e3947fb435d4b0a27d7
Ngai Wu <ucbqnhw@ucl.ac.uk>
2018-01-01 22:42:19 +0000

    Add scripts for clickable object for player to decide the 1st generation

---
+++ Assets/Chapter7_CA/Click.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Click : MonoBehaviour {
+
+    [SerializeField]
+    private LayerMask clickablesLayer;
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(Input.GetMouseButtonDown(0))
+        {
+            RaycastHit rayHit;
+
+            if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out rayHit, Mathf.Infinity, clickablesLayer))
+            {
+                rayHit.collider.GetComponent<ClickOn>().ClickMe();
+            }
+        }
+	}
+}
---
+++ Assets/Chapter7_CA/ClickOn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickOn : MonoBehaviour {
+
+    [SerializeField]
+    private Material white;
+    [SerializeField]
+    private Material black;
+
+    private MeshRenderer myRend;
+
+
+	// Use this for initialization
+	void Start () {
+        myRend = GetComponent<MeshRenderer>();
+	}
+	
+	public void ClickMe()
+    {
+        myRend.material = black;
+    }
+}
