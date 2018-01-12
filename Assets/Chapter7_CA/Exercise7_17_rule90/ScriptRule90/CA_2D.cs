52a634eea7b2dfcb6b1e67b7f19c396a1d4834ee
Will <will93611@gmail.com>
2018-01-12 23:00:00 +0000

    rule 90

--- Assets/Chapter7_CA/Exercise7_17_rule90/ScriptRule90/CA_2D.cs
+++ Assets/Chapter7_CA/Exercise7_17_rule90/ScriptRule90/CA_2D.cs
@@ -5,8 +5,43 @@ using UnityEngine;
 public class CA_2D
 {
     public bool[] Cells;
+    Texture2D _image;
 
+    Rect _rectangle;
+    int _size;
+    Color[] _colors;
+    int cells;
+    int _width;
+    int _height;
+    void Start()
+    {
+        int scale = 20;//scale up or it would be too small
+        _width = 400 / scale;
+        _height = 400 / scale;
+        _size = _width * _height;//a box
+        _image = new Texture2D(_width, _height);
+        _image.filterMode = FilterMode.Point;
+        _rectangle = new Rect(20, Screen.height - (_width * scale) - 30, _width * scale, _height * scale);//hope this would make the box center of the screen
+        _colors = new Color[_size];
+
+        for (int i = 0; i < _height; i++)
+        {
+            if (cells[i] == 0)
+            {
+                _colors[i] = Color.white;
+            }
+            else
+            {
+                _colors[i] = Color.gray;
+            }
+        }
+
+        _image.SetPixels(_colors);
+        _image.Apply();
+    }
     public CA_2D()
+
+    
     {
         Cells = new bool[41];
         Cells[20] = true;
@@ -38,5 +73,13 @@ public class CA_2D
 
         return cell;
     }
+    void Update()
+    {
+       
+    }
+private void OnGUI()
+{
+    GUI.DrawTexture(_rectangle, _image);
+}
 }
 
