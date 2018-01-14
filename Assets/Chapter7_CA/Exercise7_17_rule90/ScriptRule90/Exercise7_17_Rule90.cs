3d8f6b7c708d3ce027b119c319d43c64244aedb3
Will <will93611@gmail.com>
2018-01-14 23:54:22 +0000

    90

--- Assets/Chapter7_CA/Exercise7_17_rule90/ScriptRule90/Exercise7_17_Rule90.cs
+++ Assets/Chapter7_CA/Exercise7_17_rule90/ScriptRule90/Exercise7_17_Rule90.cs
@@ -147,4 +147,29 @@ public class Exercise7_17_Rule90 : MonoBehaviour {
             m_texture = null;
         }
     }
+    public void NextGeneration()
+    {
+        int l = Cells.Length;
+        bool[] temp = new bool[l];
+
+        for (int i = 0; i < l; i++)
+        {
+            temp[i] = NextCell(i);
+        }
+
+        Cells = temp;
+    }
+    bool NextCell(int i)
+    {
+        int l = Cells.Length;
+        bool cell = Cells[i];
+        bool left = (i == 0) ? Cells[l - 1] : Cells[i - 1];
+        bool right = (i == l - 1) ? Cells[0] : Cells[i + 1];
+
+        if (left && right) return false;
+        if (!cell && (left || right)) return true;
+        if (cell && left) return false;
+
+        return cell;
+    }
 }
